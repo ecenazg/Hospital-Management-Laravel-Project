a3fa1ffa7fadbb25d45231f72cef92b6912151bf
ecenazg <gungor.ecenaz1@gmail.com>
2023-05-18 10:05:59 +0300

    first commit

---
+++ database/migrations/2023_05_16_191741_create_doctors_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        $table = 'doctors';
+
+        if (!Schema::hasTable($table)) {
+            Schema::create($table, function (Blueprint $table) {
+                $table->id();
+                $table->string('name');
+                $table->string('specialization');
+                $table->timestamps();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::dropIfExists('doctors');
+    }
+};
