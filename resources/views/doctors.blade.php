af761953fa1a942ef8248152727b2831fad78f33
ecenazg <gungor.ecenaz1@gmail.com>
2023-05-23 22:09:42 +0300

    edit button

--- resources/views/doctors.blade.php
+++ resources/views/doctors.blade.php
@@ -95,13 +95,16 @@
                         <td>{{ $doctor->name }}</td>
                         <td>{{ $doctor->email }}</td>
                         <td>{{ $doctor->specialization }}</td>
-                        <td>
-                        
-                            <form action="{{ route('doctors.destroy', $doctor->id) }}" method="POST">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit">Delete</button>
-                            </form>
+                        <td contenteditable="true" class="edit-field">{{ $doctor->name }}</td>
+                    <td contenteditable="true" class="edit-field">{{ $doctor->email }}</td>
+                    <td contenteditable="true" class="edit-field">{{ $doctor->specialization }}</td>
+                    <td>
+                        <button class="edit-button" data-doctor-id="{{ $doctor->id }}">Edit</button>
+                        <form class="delete-form" action="{{ route('doctors.destroy', $doctor->id) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit">Delete</button>
+                        </form>
                         </td>
                     </tr>
                 @endforeach
@@ -126,5 +129,84 @@
 
         <button type="submit">Create</button>
     </form>
+    @verbatim
+    <script>
+       
+
+// Get all edit buttons and attach click event listeners
+const editButtons = document.querySelectorAll('.edit-button');
+editButtons.forEach(button => {
+    button.addEventListener('click', handleEdit);
+});
+
+// Handle the edit button click event
+function handleEdit(event) {
+    const doctorId = event.target.dataset.doctorId;
+    const editFields = document.querySelectorAll(`tr[data-doctor-id="${doctorId}"] .edit-field`);
+
+    // Enable editing on the fields
+    editFields.forEach(field => {
+        field.contentEditable = true;
+        field.classList.add('editing');
+    });
+
+    // Change the button text to 'Save'
+    event.target.textContent = 'Save';
+    event.target.removeEventListener('click', handleEdit);
+    event.target.addEventListener('click', handleSave);
+}
+
+// Handle the save button click event
+function handleSave(event) {
+    const doctorId = event.target.dataset.doctorId;
+    const editFields = document.querySelectorAll(`tr[data-doctor-id="${doctorId}"] .edit-field`);
+
+    // Disable editing on the fields
+    editFields.forEach(field => {
+        field.contentEditable = false;
+        field.classList.remove('editing');
+    });
+
+    // Change the button text back to 'Edit'
+    event.target.textContent = 'Edit';
+    event.target.removeEventListener('click', handleSave);
+    event.target.addEventListener('click', handleEdit);
+
+    // Save the changes
+    const formData = new FormData();
+    editFields.forEach(field => {
+        const columnName = field.getAttribute('data-column-name');
+        const columnValue = field.textContent;
+        formData.append(columnName, columnValue);
+    });
+
+    fetch(`/doctors/${doctorId}`, {
+        method: 'PUT', 
+        body: formData
+    })
+    .then(response => {
+        if (response.ok) {
+            console.log('Changes saved successfully.');
+        } else {
+            console.error('Failed to save changes.');
+        }
+    })
+    .catch(error => {
+        console.error('An error occurred while saving changes:', error);
+    });
+}
+
+// Prevent form submission on delete confirmation
+const deleteForms = document.querySelectorAll('.delete-form');
+deleteForms.forEach(form => {
+    form.addEventListener('submit', event => {
+        const confirmation = confirm('Are you sure you want to delete this doctor?');
+        if (!confirmation) {
+            event.preventDefault();
+        }
+    });
+});
+
+    </script>
 </body>
 </html>
