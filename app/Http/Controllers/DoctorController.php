acc4228edff5303a736f15884d65fe987372ead9
ecenazg <gungor.ecenaz1@gmail.com>
2023-06-03 15:28:58 +0300

    update

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\Doctors;
+use App\Models\Patients;
 use Illuminate\Http\Request;
 
 class DoctorController extends Controller
 {
 
-    
+
     public function index()
     {
         // Retrieve all doctors
@@ -25,8 +26,8 @@ class DoctorController extends Controller
             'email' => 'required',
         ]);
 
-        $doctor = Doctors::create($request->only('name', 'email' , 'specialization'));
-        
+        $doctor = Doctors::create($request->only('name', 'email', 'specialization'));
+
         $doctors = Doctors::orderBy('id', 'asc')->get();
         return redirect('/doctors')->with('success', 'Doctor created successfully.');
     }
@@ -39,23 +40,41 @@ class DoctorController extends Controller
             'specialization' => 'required',
         ]);
 
-       
-            $doctor = Doctors::findOrFail($id);
-            $doctor->name = $request->input('name');
-            $doctor->email = $request->input('email');
-            $doctor->specialization = $request->input('specialization');
-            $doctor->save();
-        
-            return response()->json([
-                'message' => 'Doctor updated successfully',
-                'name' => $doctor->name,
-                'email' => $doctor->email,
-                'specialization' => $doctor->specialization
-            ]);
-            
-        }
-        
-    
+
+        $doctor = Doctors::findOrFail($id);
+        $doctor->name = $request->input('name');
+        $doctor->email = $request->input('email');
+        $doctor->specialization = $request->input('specialization');
+        $doctor->save();
+
+        return response()->json([
+            'message' => 'Doctor updated successfully',
+            'name' => $doctor->name,
+            'email' => $doctor->email,
+            'specialization' => $doctor->specialization
+        ]);
+    }
+
+    public function sendToPatients(Request $request, $doctorId)
+    {
+        // Retrieve the corresponding doctor based on the $doctorId
+        $doctor = Doctors::findOrFail($doctorId);
+
+        // Retrieve the associated patients of the doctor
+        $patients = $doctor->patients;
+
+        // Prepare the patient data
+        $patientData = $patients->map(function ($patient) {
+            return [
+                'id' => $patient->id,
+                'name' => $patient->name,
+                'email' => $patient->email,
+            ];
+        });
+
+        // Return the patient data as JSON response
+        return response()->json(['patients' => $patientData]);
+    }
 
     public function destroy(int $id)
     {
