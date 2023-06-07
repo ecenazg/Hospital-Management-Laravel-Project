9ca3e17ea945f5cf0d3d327aaf54b14dbef27f1a
ecenazg <gungor.ecenaz1@gmail.com>
2023-06-07 16:11:41 +0300

    react added

--- routes/web.php
+++ routes/web.php
@@ -1,51 +1,38 @@
 <?php
 
-use App\Http\Controllers\DoctorController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PatientController;
-use App\Http\Controllers\NurseController;
-use App\Http\Controllers\ManagementController;
-use App\Http\Controllers\DepartmentController;
+use Inertia\Inertia;
 
-
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
 Route::get('/', function () {
-    return view('index');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
 
-Route::get('/doctors', [DoctorController::class, 'index']);
-Route::post('/doctors/{id}', [DoctorController::class, 'edit'])->name('doctors.edit');
-Route::post('/doctors/{doctor_id}/send-to-patients', [DoctorController::class, 'sendToPatients'])->name('doctors.sendToPatients');
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::delete('/doctors/{id}', [DoctorController::class, 'destroy'])->name('doctors.destroy');
-
-Route::get('/patients', [PatientController::class, 'index']);
-Route::post('/patients/{id}', [PatientController::class, 'edit'])->name('patients.edit');
-Route::delete('/patients/{id}', [PatientController::class, 'destroy'])->name('patients.destroy');
-
-Route::get('/nurses', [NurseController::class, 'index']);
-Route::post('/nurses/{id}', [NurseController::class, 'edit'])->name('nurses.edit');
-Route::delete('/nurses/{id}', [NurseController::class, 'destroy'])->name('nurses.destroy');
-
-
-Route::get('/management', [ManagementController::class, 'index']);
-Route::post('/management/create-patient', [ManagementController::class, 'createPatient'])->name('management.createPatient');
-Route::post('/management/create-doctor', [ManagementController::class, 'createDoctor'])->name('management.createDoctor');
-Route::post('/management/create-nurse', [ManagementController::class, 'createNurse'])->name('management.createNurse');
-
-
-Route::get('/departments', [DepartmentController::class, 'index'])->name('department.index');
-Route::get('/department/{department_name}/doctors', [DepartmentController::class , 'showDoctors'])->name('department.doctors');
-// Appointments Routes
-Route::get('/appointments', [AppointmentController::class, 'index'])->name('appointments.index');
-Route::get('/appointments/create', [AppointmentController::class, 'create'])->name('appointments.create');
-Route::post('/appointments', [AppointmentController::class, 'store'])->name('appointments.store');
-Route::get('/appointments/{appointment}', [AppointmentController::class, 'show'])->name('appointments.show');
-Route::get('/appointments/{appointment}/edit', [AppointmentController::class, 'edit'])->name('appointments.edit');
-Route::put('/appointments/{appointment}', [AppointmentController::class, 'update'])->name('appointments.update');
-Route::delete('/appointments/{appointment}', [AppointmentController::class, 'destroy'])->name('appointments.destroy');
-
-// Appointments for Doctor Routes
-Route::get('/doctors/{doctor}/appointments/create', [AppointmentController::class, 'createAppointmentForDoctor'])->name('doctors.appointments.create');
-Route::get('/doctors/{doctor}/appointments', [AppointmentController::class, 'appointmentsForDoctor'])->name('doctors.appointments');
+require __DIR__.'/auth.php';
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: 'resources/js/app.jsx',
+            ssr: 'resources/js/ssr.jsx',
             refresh: true,
         }),
+        react(),
     ],
 });
