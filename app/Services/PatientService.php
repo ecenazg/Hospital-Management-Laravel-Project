c97e721d2912db465f599af8254e9637a99bb813
ecenazg <gungor.ecenaz1@gmail.com>
2023-07-05 13:37:25 +0300

    patient service updated

--- app/Services/PatientService.php
+++ app/Services/PatientService.php
@@ -3,12 +3,13 @@
 namespace App\Services;
 
 use App\Models\Patient;
+use App\Models\Patients;
 use Exception;
 use Illuminate\Support\Facades\DB;
 
 class PatientService
 {
-    public function createPatient($patientData): Patient
+    public function createPatient($patientData): Patients
     {
         $settingService = app()->make(SettingService::class);
 
@@ -16,12 +17,12 @@ class PatientService
             DB::beginTransaction();
             $patientNumber = $settingService->getNextPatientNumber();
 
-            $patient = Patient::create([
+            $patient = Patients::create([
                 'patient_id' => now()->format('Ym') . $patientNumber,
                 'name' => $patientData['name'],
                 'phone_number' => $patientData['phone_number'],
                 'year_of_birth' => now()->subYears($patientData['age'])->format('Y'),
-                'weight' => $patientData['weight'],
+                
             ]);
 
             $settingService->incrementLastPatientNumber();
@@ -35,12 +36,12 @@ class PatientService
 
     public function updatePatient($patientData)
     {
-        $patient = Patient::find($patientData['id']);
+        $patient = Patients::find($patientData['id']);
 
         $patient->name = $patientData['name'];
         $patient->phone_number = $patientData['phone_number'];
         $patient->year_of_birth = now()->subYears($patientData['age'])->format('Y');
-        $patient->weight = $patientData['weight'];
+        
         $patient->save();
 
         return $patient;
