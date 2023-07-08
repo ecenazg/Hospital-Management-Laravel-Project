0c74a21dace9dac6d457d6a56b29400f95c9f967
ecenazg <gungor.ecenaz1@gmail.com>
2023-07-08 23:07:10 +0300

    appointment updated

--- resources/js/Pages/Appointments.jsx
+++ resources/js/Pages/Appointments.jsx
@@ -1,43 +1,57 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Link } from '@inertiajs/inertia-react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Navbar from "./Navbar";
+import { Link } from "@inertiajs/inertia-react";
 
-const Appointment = ({appointments}) => {
-  console.log(appointments);
+const Appointment = ({ appointments }) => {
+    console.log(appointments);
 
-  return (
-    <div>
-      <h1>Appointments</h1>
-      <Link href={route('appointments.add')}>Add Appointment</Link>
-      {appointments.data.length === 0 ? (
-        <p>No appointments available.</p>
-      ) : (
-        <table>
-          <thead>
-            <tr>
-              <th>Date</th>
-              <th>Type</th>
-              <th>Patient</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {appointments.data.map(appointment => (
-              <tr key={appointment.id}>
-                <td>{appointment.date}</td>
-                <td>{appointment.type}</td>
-                <td>{appointment.patient.name}</td>
-                <td>
-                  <Link href={route('appointments.view', { appointment })}>View</Link>
-                  <Link href={route('appointments.book', { patient: appointment.patient })}>Book</Link>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
-    </div>
-  );
+    return (
+        <div>
+            <Navbar />
+            <h1>Appointments</h1>
+            <Link href={route("appointments.add")}>Add Appointment</Link>
+            {appointments.data.length === 0 ? (
+                <p>No appointments available.</p>
+            ) : (
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Date</th>
+                            <th>Type</th>
+                            <th>Patient</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {appointments.data.map((appointment) => (
+                            <tr key={appointment.id}>
+                                <td>{appointment.date}</td>
+                                <td>{appointment.type}</td>
+                                <td>{appointment.patient.name}</td>
+                                <td>
+                                    <Link
+                                        href={route("appointments.view", {
+                                            appointment,
+                                        })}
+                                    >
+                                        View
+                                    </Link>
+                                    <Link
+                                        href={route("appointments.book", {
+                                            patient: appointment.patient,
+                                        })}
+                                    >
+                                        Book
+                                    </Link>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            )}
+        </div>
+    );
 };
 
 export default Appointment;
